fd4ac96a90b69a3345ed3c2817184cfbea860648
Eve-C-2 <40715528@live.napier.ac.uk>
2025-11-07 17:10:12 +0000

    edited tests to be suited for display countries via population method

---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,56 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+
+    @Test
+    void displayCountryPopulationsNull()
+    {
+        app.displayCountryPopulations(null);
+    }
+
+    @Test
+    void displayCountryPopulationsEmpty()
+    {
+        ArrayList<Country> countryArrayList = new ArrayList<Country>();
+        app.displayCountryPopulations(countryArrayList);
+    }
+
+    @Test
+    void displayCountryPopulationsContainsNull()
+    {
+        ArrayList<Country> countryArrayList = new ArrayList<Country>();
+        countryArrayList.add(null);
+        app.displayCountryPopulations(countryArrayList);
+    }
+
+    @Test
+    void printSalaries()
+    {
+        ArrayList<Country> countryArrayList = new ArrayList<Country>();
+        Country countryPopulation = new Country();
+        countryPopulation.Code = "EVE";
+        countryPopulation.Name = "Name";
+        countryPopulation.Region = "Region";
+        countryPopulation.Population = 12345;
+        countryPopulation.Continent = "Continent";
+        countryPopulation.Capital = 1;
+        countryArrayList.add(countryPopulation);
+        app.displayCountryPopulations(countryArrayList);
+    }
+}
